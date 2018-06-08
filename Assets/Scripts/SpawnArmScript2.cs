11b7e92305388adabae85f77cf499226e697f450
Lucas4185 <30445090+Lucas4185@users.noreply.github.com>
2018-06-08 20:41:19 +0200

    sjj

---
+++ Assets/Scripts/SpawnArmScript2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnArmScript2 : MonoBehaviour
+{
+
+    public GameObject Arm2;
+    bool spawnOnce = true;
+
+    Vector3 spawnPos;
+
+    void Update()
+    {
+
+        SpawnArms2();
+
+    }
+
+    // Update is called once per frame
+
+    private void SpawnArms2()
+    {
+        spawnPos.x = 10;
+        spawnPos.y = Random.Range(10f, 20f);
+        spawnPos.z = -3.47f;
+        if (spawnOnce == true)
+        {
+            Instantiate(Arm2, spawnPos, Quaternion.identity);
+            spawnOnce = false;
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnArmsScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnArmsScript : MonoBehaviour {
+
+
+    public GameObject Arm1;
+    bool spawnOnce = true;
+
+    Vector3 spawnPos;
+    // Update is called once per frame
+    void Update () {
+
+        SpawnArms();
+
+    }
+
+    private void SpawnArms()
+    {
+        spawnPos.x = -10;
+        spawnPos.y = Random.Range(5f, 20f);
+        spawnPos.z = -3.47f;
+        if (spawnOnce == true)
+        {
+            Instantiate(Arm1, spawnPos, Quaternion.identity);
+            spawnOnce = false;
+        }
+    }
+}
